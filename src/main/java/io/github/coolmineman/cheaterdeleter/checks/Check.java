b2007b3f4cd79d9ea2f06ac297c97b61227194e3
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-12-31 14:42:54 -0600

    working timer check

--- src/main/java/io/github/coolmineman/cheaterdeleter/checks/Check.java
+++ src/main/java/io/github/coolmineman/cheaterdeleter/checks/Check.java
@@ -1,9 +1,10 @@
 package io.github.coolmineman.cheaterdeleter.checks;
 
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.LiteralText;
 
 public class Check {
     public void flag(ServerPlayerEntity player, String message) {
-        //todo flag player
+        player.sendMessage(new LiteralText("Flagged: " + message), true);
     }
 }
--- src/main/java/io/github/coolmineman/cheaterdeleter/checks/TimerCheck.java
+++ src/main/java/io/github/coolmineman/cheaterdeleter/checks/TimerCheck.java
@@ -1,13 +1,48 @@
 package io.github.coolmineman.cheaterdeleter.checks;
 
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.google.common.collect.MapMaker;
+
 import io.github.coolmineman.cheaterdeleter.events.MovementPacketEvent;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 
 public class TimerCheck extends Check {
+    private static final int CHECK_PERIOD = 5;
+    private static final int MAX_PACKETS = 21 * CHECK_PERIOD; // 20 is target give some wiggle room
+    private ConcurrentMap<ServerPlayerEntity, PlayerTimerInfo> map = new MapMaker().weakKeys().makeMap();
+
     public TimerCheck() {
         MovementPacketEvent.EVENT.register((player, packet) -> {
-            System.out.println(packet.isOnGround());
+            if (!map.containsKey(player)) map.put(player, new PlayerTimerInfo());
+            PlayerTimerInfo info = map.get(player);
+            info.movementPackets.addAndGet(1);
             return ActionResult.PASS;
         });
+
+        ServerTickEvents.END_SERVER_TICK.register(server -> {
+            for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
+                if (map.containsKey(player)) {
+                    PlayerTimerInfo timerInfo = map.get(player);
+                    long timediff = System.currentTimeMillis() - timerInfo.time;
+                    if (timediff > 1000 * CHECK_PERIOD) {
+                        int movementPackets = timerInfo.movementPackets.getAndSet(0);
+                        timerInfo.time = System.currentTimeMillis();
+                        System.out.println(movementPackets);
+                        if (movementPackets > MAX_PACKETS) {
+                            flag(player, "Failed Timer Check");
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private class PlayerTimerInfo {
+        public AtomicInteger movementPackets = new AtomicInteger(0);
+        public long time = System.currentTimeMillis();
     }
 }
