e10f7709605e66c527c3c64e9024a09df7015c23
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2021-01-01 17:52:05 -0600

    fix onGround slab edge case

--- src/main/java/io/github/coolmineman/cheaterdeleter/checks/OnGroundVerticalCheck.java
+++ src/main/java/io/github/coolmineman/cheaterdeleter/checks/OnGroundVerticalCheck.java
@@ -16,12 +16,18 @@ public class OnGroundVerticalCheck extends Check implements MovementPacketCallba
     @Override
     public ActionResult onMovementPacket(ServerPlayerEntity player, PlayerMoveC2SPacketView packet) {
         // System.out.println(System.currentTimeMillis() - TrackerManager.get(PlayerHitGroundData.class, player).lastHitGround.get());
-        if (packet.isChangePosition() && packet.isOnGround() && player.isOnGround() && System.currentTimeMillis() - TrackerManager.get(PlayerHitGroundData.class, player).lastInAir.get() > 500) {
-            boolean nearNonSolid = CollisionUtil.isNearby(player, CollisionUtil.NON_SOLID_COLLISION);
-            if (!nearNonSolid) {
-                double ydelta = packet.getY() - player.getY();
-                if (ydelta > 0.3 || ydelta < -0.9) flag(player, "Player Moved Vertically While onGround " + ydelta);
-            }
+        if (packet.isChangePosition() &&
+            packet.isOnGround() &&
+            player.isOnGround() &&
+            System.currentTimeMillis() - TrackerManager.get(PlayerHitGroundData.class, player).lastInAir.get() > 500 &&
+            !CollisionUtil.isNearby(player, CollisionUtil.NON_SOLID_COLLISION) &&
+            !CollisionUtil.isTouching(player, packet.getX(), packet.getY(), packet.getZ(), CollisionUtil.STEPABLE)
+        ) {
+            double ydelta = packet.getY() - player.getY();
+            if (ydelta > 0.3)
+                flag(player, "Player Moved Vertically While onGround " + ydelta);
+            if (ydelta < -0.9)
+                flag(player, "Player Moved Vertically While onGround " + ydelta);
         }
         return ActionResult.PASS;
     }
--- src/main/java/io/github/coolmineman/cheaterdeleter/util/CollisionUtil.java
+++ src/main/java/io/github/coolmineman/cheaterdeleter/util/CollisionUtil.java
@@ -1,19 +1,25 @@
 package io.github.coolmineman.cheaterdeleter.util;
 
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
+
+import org.apache.commons.lang3.mutable.MutableBoolean;
 
 import net.minecraft.block.BedBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SlimeBlock;
+import net.minecraft.entity.EntityPose;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
+import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.World;
 
 public class CollisionUtil {
     private CollisionUtil() { }
 
-    public static final Predicate<BlockState> NON_SOLID_COLLISION = state -> {
+    public static final BiPredicate<World, BlockPos> NON_SOLID_COLLISION = (world, pos) -> {
+        BlockState state = world.getBlockState(pos);
         if (!state.getFluidState().isEmpty()) return true;
         if (state.getMaterial().isLiquid()) return true;
         if (state.getBlock() instanceof SlimeBlock) return true;
@@ -21,11 +27,32 @@ public class CollisionUtil {
         return false;
     };
 
+    public static final BiPredicate<World, BlockPos> STEPABLE = (world, pos) -> {
+        VoxelShape shape = world.getBlockState(pos).getCollisionShape(world, pos);
+        MutableBoolean steppable = new MutableBoolean(false);
+        shape.forEachBox((minX, minY, minZ, maxX, maxY, maxZ) -> {
+            if (maxY < 0.51) steppable.setTrue();
+        });
+        return steppable.booleanValue();
+    };
+
     public static boolean isNearby(ServerPlayerEntity player, Predicate<BlockState> predicate) {
-        return isNearby(player.getBoundingBox().expand(2.0, 4.0, 2.0), player.world, predicate);
+        return isTouching(player.getBoundingBox().expand(2.0, 4.0, 2.0), player.world, predicate);
     }
 
-    public static boolean isNearby(Box box, World world, Predicate<BlockState> predicate) {
-        return BlockPos.stream(box).anyMatch(pos -> predicate.test(world.getBlockState(pos)));
+    public static boolean isNearby(ServerPlayerEntity player, BiPredicate<World, BlockPos> predicate) {
+        return isTouching(player.getBoundingBox().expand(2.0, 4.0, 2.0), player.world, predicate);
+    }
+
+    public static boolean isTouching(ServerPlayerEntity player, double posx, double posy, double posz, BiPredicate<World, BlockPos> predicate) {
+        return isTouching(player.getDimensions(EntityPose.STANDING).method_30231(posx, posy, posz).expand(0.4), player.world, predicate); // method_30231 -> withPos
+    }
+
+    public static boolean isTouching(Box box, World world, Predicate<BlockState> predicate) {
+        return isTouching(box, world, (world1, pos) -> predicate.test(world1.getBlockState(pos)));
+    }
+
+    public static boolean isTouching(Box box, World world, BiPredicate<World, BlockPos> predicate) {
+        return BlockPos.stream(box).anyMatch(pos -> predicate.test(world, pos));
     }
 }
