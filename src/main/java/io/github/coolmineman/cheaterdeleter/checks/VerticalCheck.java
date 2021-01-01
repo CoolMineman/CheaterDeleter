34656393a0f887a1054bf0f74794bfa04a1fdc0b
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-12-31 18:36:52 -0600

    improve VerticalCheck

--- src/main/java/io/github/coolmineman/cheaterdeleter/checks/VerticalCheck.java
+++ src/main/java/io/github/coolmineman/cheaterdeleter/checks/VerticalCheck.java
@@ -17,18 +17,21 @@ public class VerticalCheck extends Check implements MovementPacketCallback {
             PlayerDataManager.put(player, VerticalCheckData.class, null);
             return ActionResult.PASS;
         }
+        VerticalCheckData verticalCheckData = PlayerDataManager.getOrCreate(player, VerticalCheckData.class, VerticalCheckData::new);
         if (player.isOnGround() && !packet.isOnGround() && player.getVelocity().getY() < 0.45) {
-            VerticalCheckData verticalCheckData = PlayerDataManager.getOrCreate(player, VerticalCheckData.class, VerticalCheckData::new);
             verticalCheckData.maxY = player.getY() + 1.45;
             verticalCheckData.isActive = true;
         } else if (packet.isOnGround()) {
-            VerticalCheckData verticalCheckData = PlayerDataManager.get(player, VerticalCheckData.class);
             if (verticalCheckData != null && verticalCheckData.isActive) {
                 verticalCheckData.isActive = false;
             }
         } else { //Packet off ground
-            VerticalCheckData verticalCheckData = PlayerDataManager.get(player, VerticalCheckData.class);
-            if (verticalCheckData != null && verticalCheckData.isActive && packet.getY(player.getY()) > verticalCheckData.maxY) flag(player, "Failed Vertical Movement Check");
+            if (verticalCheckData.isActive && packet.getY(player.getY()) > verticalCheckData.maxY) flag(player, "Failed Vertical Movement Check");
+            if (!verticalCheckData.isActive && player.getVelocity().getY() < 0.45) {
+                verticalCheckData.maxY = player.getY() + 1.45;
+                verticalCheckData.isActive = true;
+            }
+
         }
         return ActionResult.PASS;
     }
