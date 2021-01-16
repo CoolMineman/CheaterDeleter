25cbe966c5d368296fe67bf8547b90e0c493787e
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2021-01-16 14:15:36 -0600

    remove unused event

--- src/main/java/io/github/coolmineman/cheaterdeleter/events/EventManager.java
+++ src/main/java/io/github/coolmineman/cheaterdeleter/events/EventManager.java
@@ -8,6 +8,5 @@ public class EventManager {
         PlayerEndTickCallback.init();
         OutgoingTeleportListener.init();
         ClickSlotC2SPacketCallback.init();
-        VehicleMoveC2SPacketCallback.init();
     }
 }
