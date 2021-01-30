202fb925d3bf996b03d5e6983d4a9fb5a24e56ce
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2021-01-29 23:43:11 -0600

    entity timer + fix stupid threading issue that is stupid ahhhhhhhhhhhhhhhhh

---
+++ src/main/java/io/github/coolmineman/cheaterdeleter/modules/movement/entity/EntityTimerCheck.java
@@ -0,0 +1,55 @@
+package io.github.coolmineman.cheaterdeleter.modules.movement.entity;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.jetbrains.annotations.Nullable;
+
+import io.github.coolmineman.cheaterdeleter.events.PlayerEndTickCallback;
+import io.github.coolmineman.cheaterdeleter.events.VehicleMoveListener;
+import io.github.coolmineman.cheaterdeleter.modules.CDModule;
+import io.github.coolmineman.cheaterdeleter.objects.PlayerMoveC2SPacketView;
+import io.github.coolmineman.cheaterdeleter.objects.entity.CDEntity;
+import io.github.coolmineman.cheaterdeleter.objects.entity.CDPlayer;
+import net.minecraft.network.packet.c2s.play.PlayerInputC2SPacket;
+import net.minecraft.network.packet.c2s.play.VehicleMoveC2SPacket;
+
+public class EntityTimerCheck extends CDModule implements VehicleMoveListener, PlayerEndTickCallback {
+    private static final int CHECK_PERIOD = 5;
+    private static final int MAX_PACKETS = 21 * CHECK_PERIOD; // 20 is target give some wiggle room
+
+    public EntityTimerCheck() {
+        super("entity_timer_check");
+        VehicleMoveListener.EVENT.register(this);
+        PlayerEndTickCallback.EVENT.register(this);
+    }
+
+    private class EntityTimerInfo {
+        public AtomicInteger packets = new AtomicInteger(0);
+        public long time = System.currentTimeMillis();
+    }
+
+    @Override
+    public void onVehicleMove(CDPlayer player, CDEntity vehicle, PlayerMoveC2SPacketView playerLook,
+            PlayerInputC2SPacket playerInput, VehicleMoveC2SPacket vehicleMoveC2SPacket,
+            @Nullable VehicleMoveC2SPacket lastVehicleMoveC2SPacket) {
+        if (!enabledFor(player)) return;
+        EntityTimerInfo info = player.getOrCreateData(EntityTimerInfo.class, EntityTimerInfo::new);
+        info.packets.addAndGet(1);
+    }
+
+    @Override
+    public void onPlayerEndTick(CDPlayer player) {
+        if (!enabledFor(player)|| player.getVehicleCd() != null) return;
+        EntityTimerInfo info = player.getData(EntityTimerInfo.class);
+        if (info != null) {
+            long timediff = System.currentTimeMillis() - info.time;
+            if (timediff > 1000 * CHECK_PERIOD) {
+                int movementPackets = info.packets.getAndSet(0);
+                info.time = System.currentTimeMillis();
+                if (movementPackets > MAX_PACKETS) {
+                    if (flag(player, FlagSeverity.MINOR, "Failed Entity Timer Check")) player.rollback();
+                }
+            }
+        }
+    }
+}
