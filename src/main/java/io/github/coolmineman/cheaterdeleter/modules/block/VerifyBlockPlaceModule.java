86cea43e59cb42ab4318c8a36a1d4838f30c6aad
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2021-06-10 14:37:52 -0500

    allow placing blocks again

--- src/main/java/io/github/coolmineman/cheaterdeleter/modules/block/VerifyBlockPlaceModule.java
+++ src/main/java/io/github/coolmineman/cheaterdeleter/modules/block/VerifyBlockPlaceModule.java
@@ -5,16 +5,20 @@ import io.github.coolmineman.cheaterdeleter.modules.CDModule;
 import io.github.coolmineman.cheaterdeleter.objects.entity.CDPlayer;
 import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
 
 public class VerifyBlockPlaceModule extends CDModule implements PlayerInteractBlockCallback {
     public VerifyBlockPlaceModule() {
         super("verify_block_place");
+        PlayerInteractBlockCallback.EVENT.register(this);
     }
 
     @Override
     public boolean onPlayerInteractBlock(CDPlayer player, PlayerInteractBlockC2SPacket packet) {
         BlockHitResult hr = packet.getBlockHitResult();
-        return !enabledFor(player) || !player.getWorld().getBlockState(hr.getBlockPos()).getOutlineShape(player.getWorld(), hr.getBlockPos()).isEmpty();
+        BlockPos pos = hr.getBlockPos();
+        if (hr.isInsideBlock()) pos = pos.offset(hr.getSide());
+        return !player.getWorld().getBlockState(pos).getOutlineShape(player.getWorld(), pos).isEmpty();
     }
 
 }
